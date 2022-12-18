0d1d467f9280adc4c48d773215590dc821e54ee8
Dennis Jüni <jueni@ubique.ch>
2022-12-18 17:15:13 +0100

    solve day 5 part 1

---
+++ 05/part1.ts
@@ -0,0 +1,48 @@
+import * as fs from 'fs';
+
+const solve = () => {
+  let stacks = Array(9);
+  stacks[0] = ['N', 'T', 'B', 'S', 'Q', 'H', 'G', 'R'].reverse();
+  stacks[1] = ['J', 'Z', 'P', 'D', 'F', 'S', 'H'].reverse();
+  stacks[2] = ['V', 'H', 'Z'].reverse();
+  stacks[3] = ['H', 'G', 'F', 'J', 'Z', 'M'].reverse();
+  stacks[4] = ['R', 'S', 'M', 'L', 'D', 'C', 'Z', 'T'].reverse();
+  stacks[5] = ['J', 'Z', 'H', 'V', 'W', 'T', 'M'].reverse();
+  stacks[6] = ['Z', 'L', 'P', 'F', 'T'].reverse();
+  stacks[7] = ['S', 'W', 'V', 'Q'].reverse();
+  stacks[8] = ['C', 'N', 'D', 'T', 'M', 'L', 'H', 'W'].reverse();
+
+  const input = fs.readFileSync('./05/input.txt', 'utf8');
+
+  for (const line of input.split('\n')) {
+    const { count, from, to } = parseCommand(line);
+    for (let i = 0; i < count; i++) {
+      let popped = stacks[from - 1].pop();
+      stacks[to - 1].push(popped);
+    }
+  }
+
+  let str = '';
+  for (let i = 0; i < stacks.length; i++) {
+    str += stacks[i].at(-1);
+  }
+
+  console.log(str);
+};
+
+export interface Command {
+  count: number;
+  from: number;
+  to: number;
+}
+
+const parseCommand = (rawCommand: string): Command => {
+  const splitLine = rawCommand.split(' ');
+  return {
+    count: parseInt(splitLine[1]),
+    from: parseInt(splitLine[3]),
+    to: parseInt(splitLine[5]),
+  } as Command;
+};
+
+solve();
