80b9d583ed109b59f1037477b2aa0bec9dafb504
Dennis Jüni <dennis.01@hispeed.ch>
2022-12-20 00:37:15 +0100

    solve day 9

--- 09/part1.ts
+++ 09/part1.ts
@@ -2,6 +2,59 @@ import * as fs from 'fs';
 
 const solve = () => {
   const input = fs.readFileSync('./09/input.txt', 'utf8');
+
+  let splitInput = input.split('\n');
+
+  let set = new Set();
+
+  let headPos = new Point(0, 0);
+  let tailPos = new Point(0, 0);
+  set.add(`${tailPos.x},${tailPos.y}`);
+
+  for (let i = 0; i < splitInput.length; i++) {
+    const direction = splitInput[i].split(' ')[0];
+    const amount = parseInt(splitInput[i].split(' ')[1]);
+
+    for (let j = 0; j < amount; j++) {
+      switch (direction) {
+        case 'R':
+          headPos.x++;
+          break;
+        case 'L':
+          headPos.x--;
+          break;
+        case 'D':
+          headPos.y++;
+          break;
+        case 'U':
+          headPos.y--;
+          break;
+      }
+
+      let notTouching =
+        Math.abs(headPos.x - tailPos.x) > 1 ||
+        Math.abs(headPos.y - tailPos.y) > 1;
+
+      if (notTouching) {
+        //catching up is equal to adding the sign of the difference to the head position
+        tailPos.x += Math.sign(headPos.x - tailPos.x);
+        tailPos.y += Math.sign(headPos.y - tailPos.y);
+        set.add(`${tailPos.x},${tailPos.y}`);
+      }
+    }
+  }
+
+  let visitedCount = set.size;
+  console.log(visitedCount);
 };
+
+class Point {
+  public x: number;
+  public y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 solve();
--- 09/part2.ts
+++ 09/part2.ts
@@ -2,6 +2,68 @@ import * as fs from 'fs';
 
 const solve = () => {
   const input = fs.readFileSync('./09/input.txt', 'utf8');
+
+  let splitInput = input.split('\n');
+
+  let set = new Set();
+
+  //Learning: setting rope = Array(10).fill(new Point(0,0)) initializes all 10 elements in the array to the same object. Changing one of the objects changes all of them.
+  let rope = Array(10)
+    .fill(0)
+    .map(() => {
+      return new Point(0, 0);
+    });
+
+  set.add(`0,0`);
+
+  for (let i = 0; i < splitInput.length; i++) {
+    const direction = splitInput[i].split(' ')[0];
+    const amount = parseInt(splitInput[i].split(' ')[1]);
+
+    for (let j = 0; j < amount; j++) {
+      switch (direction) {
+        case 'R':
+          rope[0].x++;
+          break;
+        case 'L':
+          rope[0].x--;
+          break;
+        case 'D':
+          rope[0].y++;
+          break;
+        case 'U':
+          rope[0].y--;
+          break;
+      }
+
+      for (let k = 1; k < rope.length; k++) {
+        let notTouching =
+          Math.abs(rope[k - 1].x - rope[k].x) > 1 ||
+          Math.abs(rope[k - 1].y - rope[k].y) > 1;
+
+        if (notTouching) {
+          //catching up is equal to adding the sign of the difference to the head position
+          rope[k].x += Math.sign(rope[k - 1].x - rope[k].x);
+          rope[k].y += Math.sign(rope[k - 1].y - rope[k].y);
+          if (k === rope.length - 1) {
+            set.add(`${rope[k].x},${rope[k].y}`);
+          }
+        }
+      }
+    }
+  }
+
+  let visitedCount = set.size;
+  console.log(visitedCount);
 };
+
+class Point {
+  public x: number;
+  public y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 solve();
